1f6602ee065ba004cffba41f62f6b3f1e3fd388e
capitanfight <119853063+capitanfight@users.noreply.github.com>
2023-05-21 14:22:28 +0000

    stile scelta difficolta'

--- js/difficolta.js
+++ js/difficolta.js
@@ -1,11 +1,16 @@
 import { mine } from "./script.js";
 
 const PULSANTE_SETTING = document.getElementById('setting');
+const PULSANTE_CLOSE_SETTING = document.getElementById('close');
 let stato = false;
-PULSANTE_SETTING.addEventListener('click', e => {
+
+const apriChiudi = () => {
     PULSANTE_SETTING.dataset.stato = `${!stato}`;
     stato = !stato;
-})
+}
+
+PULSANTE_SETTING.addEventListener('click', apriChiudi);
+PULSANTE_CLOSE_SETTING.addEventListener('click', apriChiudi);
 
 const DIFF_EZ = document.getElementById('ez');
 const DIFF_MID = document.getElementById('mid');
@@ -13,10 +18,19 @@ const DIFF_HARD = document.getElementById('hard');
 
 export function cambioDiff(e) {
     if (e.target === DIFF_EZ) {
-        mine.setMine(35)
+        mine.setMine(35);
+        DIFF_EZ.classList.add('selezionato');
+        DIFF_MID.classList.remove('selezionato');
+        DIFF_HARD.classList.remove('selezionato');
     } else if (e.target === DIFF_MID) {
-        mine.setMine(65)
+        mine.setMine(65);
+        DIFF_MID.classList.add('selezionato');
+        DIFF_EZ.classList.remove('selezionato');
+        DIFF_HARD.classList.remove('selezionato');
     } else if (e.target === DIFF_HARD) {
-        mine.setMine(95)
+        mine.setMine(95);
+        DIFF_HARD.classList.add('selezionato');
+        DIFF_MID.classList.remove('selezionato');
+        DIFF_EZ.classList.remove('selezionato');
     }
 }
